97c0971b2ad80d0e925876db6772aba6922423b3
Juan Jose Alejo <juanjose.alejofalcon@nttdata.com>
2022-04-19 11:03:32 -0500

    Microservicio para Cuentas de Ahorro

---
+++ src/test/java/com/bootcamp/savingaccount/SavingaccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.savingaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingaccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
